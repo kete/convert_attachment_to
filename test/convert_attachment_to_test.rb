a401fcc6db773097f2b1285a08f241a05be39ed7
Walter McGinnis <walter@katipo.co.nz>
2007-11-13 20:36:09 +0000

    added html to tests

--- test/convert_attachment_to_test.rb
+++ test/convert_attachment_to_test.rb
@@ -43,6 +43,24 @@ class ConvertAttachmentToTest < Test::Unit::TestCase
     assert_equal File.read(File.join(File.dirname(__FILE__), 'fixtures/files/msword_to_text.txt')), to_text_doc.description, "convert_attachment_to plugin: pdf to text results in unexpected value."
   end
 
+  # test methods that do the conversions
+  def test_convert_from_html_to_html
+    to_html_doc = DocumentToHtml.new(:title => 'test document',
+                                     :uploaded_data => fixture_file_upload('/files/test.html', 'text/html'))
+    to_html_doc.save
+    to_html_doc.reload
+
+    assert_equal File.read(File.join(File.dirname(__FILE__), 'fixtures/files/html_to_html.html')), to_html_doc.description, "convert_attachment_to plugin: html to html results in unexpected value."
+  end
+
+  def test_convert_from_html_to_text
+    to_text_doc = DocumentToText.new(:title => 'test document',
+                                     :uploaded_data => fixture_file_upload('/files/test.html', 'text/html'))
+    to_text_doc.save
+    to_text_doc.reload
+
+    assert_equal File.read(File.join(File.dirname(__FILE__), 'fixtures/files/to_text.txt')), to_text_doc.description, "convert_attachment_to plugin: html to text results in unexpected value."
+  end
 
   # test that uploaded file gets added to proper attribute in proper form
 end
